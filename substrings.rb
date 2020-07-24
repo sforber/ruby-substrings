55014fdaf7afc20a77b37bb4e4747991948b9453
Scott Forber <scottforber@gmail.com>
2020-07-24 13:05:45 +0100

    Add substrings.rb

---
+++ substrings.rb
@@ -0,0 +1,17 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(message, dictionary)
+    hash = Hash.new(0)
+    message.downcase!
+    messageArray = message.split(/[^[[:word:]]]+/)
+    dictionary.each do |checkWord|
+        messageArray.each do |word|
+            if word.include?(checkWord)
+                hash[checkWord] += 1
+            end
+        end
+    end
+    puts hash
+end
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
